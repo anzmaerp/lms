621d8a918ffe16feceadc56b8392dd7608145a56
anzmaerp <anzmamaindevice@gmail.com>
2025-07-07 18:22:07 +0300

    chore: add new modules and assets for IPManager, Upcertify, Assignments, and CourseBundles
    feat(IPManager): initialize IPManager module with config, models, and views
    feat(Upcertify): set up Upcertify module with assets, components, and layouts
    feat(Assignments): create Assignments module with models and resources
    feat(CourseBundles): establish CourseBundles module with assets and views
    style: update modules_statuses.json to include new modules

---
+++ Modules/CourseBundles/Models/CourseBundle.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\CourseBundles\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Courses\Models\Course;
+
+class CourseBundle extends Model
+{
+    protected $table;
+    public $timestamps = false;
+
+    public function __construct()
+    {
+        $this->table = (config('coursebundles.db_prefix') ?? 'courses_') . 'course_bundles';
+    }
+
+
+    protected $fillable = [
+        'bundle_id',
+        'course_id',
+    ];
+
+    /**
+     * Get the instructor for the bundle.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+    public function bundle(): BelongsTo
+    {
+        return $this->belongsTo(Bundle::class, 'bundle_id');
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+}
