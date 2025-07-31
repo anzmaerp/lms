95348c1df88264f0aa0d6141baee260437934abf
anzmaerp <anzmamaindevice@gmail.com>
2025-07-31 14:13:39 +0300

    chore: update gitignore and remove outdated SQL file
    - Add /database/DB/* to gitignore to exclude database files
    - Remove youssef_changes.sql as it appears to be outdated and no longer needed

--- database/DB/youssef_changes.sql
+++
@@ -1,8 +0,0 @@
-alter table orders 
-add column  fawaterk_status longtext DEFAULT null;
-
-ALTER TABLE coupons 
-ADD COLUMN instructor_id BIGINT(20) UNSIGNED NULL,
-ADD CONSTRAINT coupons_instructor_id_fk FOREIGN KEY (instructor_id) REFERENCES users(id);
-
--- Taken By Mohab Bahlol Office@work
