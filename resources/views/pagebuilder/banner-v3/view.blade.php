e02c69822ed8d5efd6938dc029376d5bfae3d579
anzmaerp <anzmamaindevice@gmail.com>
2025-08-16 22:30:06 +0300

    fix: remove conditional check for whatsapp banner display
    The whatsapp banner should always be displayed regardless of phone number availability to ensure users can always access support

--- resources/views/pagebuilder/banner-v3/view.blade.php
+++ resources/views/pagebuilder/banner-v3/view.blade.php
@@ -1,14 +1,12 @@
 @php
     $phone = setting('_general.technical_support_whatsapp_number');
 @endphp
-@if(!empty($phone))
 <a href="https://wa.me/{{ setting('_general.technical_support_whatsapp_number') }}" 
     class="whatsapp-float" 
     target="_blank" 
     rel="noopener noreferrer">
         <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
 </a>
-@endif
 <div class="am-banner-potential am-banner-content-three">
     <div class="container">
         <div class="row">
