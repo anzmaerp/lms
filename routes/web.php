b48777abfb99ed3371861a8f28e1c0cf6a077678
anzmaerp <anzmamaindevice@gmail.com>
2025-09-25 13:17:31 +0300

    just push for bperfect

--- routes/web.php
+++ routes/web.php
@@ -162,5 +162,7 @@ Route::middleware(['locale', 'maintenance'])->group(function () {
     if (!request()->is('api/*')) {
         require __DIR__ . '/pagebuilder.php';
     }
+    
     // route::get('home-four',[HomefourController::class,'index','index'])->name('home4');
+
 });
