52028475562ec9e689113d4853e0ed9b34dc1db0
hawary-anzma <3hand.backend2@gmail.com>
2025-10-23 13:09:40 +0300

    feat(course-bundles): add dynamic currency symbol support for pricing
    Implement dynamic currency symbol display in course bundle pricing based on system settings. The change includes:
    - Fetching currency symbol from system settings
    - Passing the symbol to the view
    - Updating blade templates to use dynamic symbol instead of hardcoded '$'

--- Modules/CourseBundles/Livewire/Pages/Search/SearchCoursesBundles.php
+++ Modules/CourseBundles/Livewire/Pages/Search/SearchCoursesBundles.php
@@ -2,14 +2,14 @@
 
 namespace Modules\CourseBundles\Livewire\Pages\Search;
 
-use Modules\CourseBundles\Models\Bundle;
-use Modules\CourseBundles\Services\BundleService;
 use App\Models\Language;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Modules\CourseBundles\Models\Bundle;
+use Modules\CourseBundles\Services\BundleService;
 
 class SearchCoursesBundles extends Component
 {
@@ -21,36 +21,41 @@ class SearchCoursesBundles extends Component
     public $parPageList = [10, 25, 50, 100];
 
     public $filters = [
-        'keyword'   => '',
-        'min_price'         => null,
-        'max_price'         => null,
-        'statuses'  => [Bundle::STATUS_PUBLISHED]
+        'keyword' => '',
+        'min_price' => null,
+        'max_price' => null,
+        'statuses' => [Bundle::STATUS_PUBLISHED]
     ];
 
-    
     public function mount()
     {
         $this->perPage = setting('_general.per_page_record') ?? 10;
     }
-    
+
     public function boot(BundleService $bundleService)
     {
         $this->bundleService = $bundleService;
-
     }
 
     #[Layout('layouts.frontend-app')]
     public function render()
     {
         $bundles = $this->bundleService->getBundles(
-            with: ['thumbnail:mediable_id,mediable_type,type,path','instructor.profile'],
+            with: ['thumbnail:mediable_id,mediable_type,type,path', 'instructor.profile'],
             withCount: ['courses'],
             withSum: ['courses' => 'content_length'],
             filters: $this->filters,
             perPage: $this->perPage
         );
+        $currency = setting('_general.currency');
+        $currency_detail = !empty($currency) ? currencyList($currency) : array();
 
-        return view('coursebundles::livewire.search.search-courses-bundles', compact('bundles'));
+        if (!empty($currency_detail['symbol'])) {
+            $this->currency_symbol = $currency_detail['symbol'];
+        }
+        $currency_symbol = $this->currency_symbol;
+
+        return view('coursebundles::livewire.search.search-courses-bundles', compact('bundles', 'currency_symbol'));
     }
 
     public function loadData()
@@ -60,9 +65,7 @@ class SearchCoursesBundles extends Component
     }
 
     public function updatedPerPage()
-    {   
-        $this->resetPage(); 
+    {
+        $this->resetPage();
     }
-    
-
 }
